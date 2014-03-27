2faca0b11647d277d493102010d2762c1bb8ea81
ybr <ybr@zenexity.com>
2014-03-27 06:11:51 +0100

    Test tooling

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,24 @@
 module.exports = function(grunt) {
+  var browsers = [{
+    browserName: "firefox",
+    version: "19",
+    platform: "XP"
+  },{
+  //   browserName: "chrome",
+  //   platform: "XP"
+  // }, {
+  //   browserName: "chrome",
+  //   platform: "linux"
+  // }, {
+    browserName: "internet explorer",
+    version: "10",
+    platform: "WIN8"
+  // }, {
+  //   browserName: "internet explorer",
+  //   version: "9",
+  //   platform: "VISTA"
+  }];
+
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
     jshint: {
@@ -30,21 +50,38 @@ module.exports = function(grunt) {
         }
       }
     },
-    mochaTest: {
-      test: {
+    connect: {
+      server: {
         options: {
-          reporter: 'spec'
-        },
-        src: ['test/**/*.js']
+          base: "",
+          port: 9999
+        }
+      }
+    },
+    watch: {},
+    'saucelabs-mocha': {
+      all: {
+        options: {
+          urls: ["http://127.0.0.1:9999/test/index.html"],
+          tunnelTimeout: 5,
+          build: process.env.TRAVIS_JOB_ID,
+          concurrency: 3,
+          browsers: browsers,
+          testname: 'fundomplate',
+          tags: ['master']
+        }
       }
     }
   })
 
   grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks("grunt-contrib-connect")
+  grunt.loadNpmTasks("grunt-contrib-watch")
   grunt.loadNpmTasks('grunt-string-replace')
-  grunt.loadNpmTasks('grunt-mocha-test')
+  grunt.loadNpmTasks("grunt-saucelabs")
 
-  grunt.registerTask('default', ['mochaTest', 'jshint', 'string-replace', 'uglify'])
-  grunt.registerTask('test', ['mochaTest'])
+  grunt.registerTask('default', ['jshint', 'string-replace', 'uglify'])
+  grunt.registerTask('test', ['connect', 'saucelabs-mocha'])
+  grunt.registerTask('dev', ['connect', 'watch'])
 }
--- README.md
+++ README.md
@@ -4,3 +4,16 @@ fundomplate
 Pre-alpha release
 
 Stateful functional DOM templating
+
+Test
+====
+
+Mocha + saucelabs
+
+https://saucelabs.com/docs/javascript-unit-testing-tutorial
+
+Set up environment variables that will store your Sauce username and access key:
+$ export SAUCE_USERNAME=YOUR-SAUCE-USERNAME
+$ export SAUCE_ACCESS_KEY=YOUR-SAUCE-API-KEY
+
+$ grunt test
